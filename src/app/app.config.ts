846185861f6a807fd9d06bf161ba57991d2e691c
Prasad Jadhav <pjadhav9797@gmail.com>
2024-09-04 14:57:31 -0400

    installed packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
- 
-  ]
+  ],
 };
